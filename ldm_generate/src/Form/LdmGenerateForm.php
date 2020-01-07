5f12887caa661dc3834ca99d9855ae789f5ddcfe
Tom <dominik.thomasdl@gmail.com>
2020-01-08 00:36:30 +0100

    Modification du module de generation de lettre

--- ldm_generate/src/Form/LdmGenerateForm.php
+++ ldm_generate/src/Form/LdmGenerateForm.php
@@ -14,7 +14,38 @@ class LdmGenerateForm extends FormBase {
   public function buildForm(array $form, FormStateInterface $form_state){
     $form['name'] = [
       '#type' => 'textfield',
-      '#title' => $this->t('Name'),
+      '#title' => $this->t('Nom de l\'entreprise'),
+    ];
+
+    $form['adresse'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Adresse'),
+    ];
+
+    $form['cp'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Code postal'),
+    ];
+
+    $form['ville'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Ville'),
+    ];
+
+    $form['contact'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Nom du contact'),
+    ];
+
+    $form['poste'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Poste recherché'),
+    ];
+
+    $form['type'] = [
+      '#type' => 'select',
+      '#options' => $this->getTypes(),
+      '#title' => $this->t('Type de candidature'),
     ];
 
     $form['submit'] = [
@@ -27,6 +58,13 @@ class LdmGenerateForm extends FormBase {
 
   public function submitForm(array &$form, FormStateInterface $form_state){
 
+  }
+
+  public function getTypes() {
+    return [
+      'spontanee' => 'Spontanée',
+      'online' => 'Offre',
+    ];
   }
 
 }
